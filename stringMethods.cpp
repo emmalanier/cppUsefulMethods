d8dbe086497f7d0ffa82910a1d78ec542a73be55
emmalanier <emma.lanier@outlook.fr>
2025-10-15 18:26:33 +0200

    isIn method

--- stringMethods.cpp
+++ stringMethods.cpp
@@ -27,3 +27,20 @@ void myString::setMyStringVar(const std::string & str)
 {
   myStringVar = str ;
 }
+
+bool myString::isIn(const std::vector <std::string> & vec)
+{
+  bool results = false;
+
+  for(int i=0; i<vec.size(); i++)
+    {
+      if(myStringVar==vec[i])
+        {
+          results = true;
+          break;
+        }
+    }
+
+  return results ;
+
+}
